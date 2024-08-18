3c71bf108b2c059f95010a1813bc25fb36ede1be
Arbaz Hamdani <Arbazhamdani79@gmail.com>
2024-08-18 05:37:46 -0700

    My Java Script Practice

---
+++ app.js
@@ -0,0 +1,17 @@
+var firstName = ""
+firstName = "Arbaz"
+console.log(firstName)
+
+var LastName = ""
+LastName = "Hamdani"
+console.log(LastName)
+
+var Address = "Flat # c401 , Al Rehman Garden"
+console.log(Address)
+
+Address = "Garden West Karachi"
+console.log(Address)
+
+
+
+
